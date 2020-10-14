43579a7f245cf890c775ec1bf2995f6068466e9e
Naor Farcas <naor@korentec.co.il>
2020-10-14 10:41:55 +0300

    Add project files.

---
+++ KTT/Controllers/UsersController.cs
@@ -0,0 +1,31 @@
+﻿using KTT.Data;
+using KTT.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace KTT.Controllers
+{
+    public class UsersController : BaseController
+    {
+        private readonly DataContext _context;
+
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            return await _context.Users.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> GetUser(int id)
+        {
+            return await _context.Users.FindAsync(id);
+        }
+    }
+}
---
+++ KTT/Data/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using KTT.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace KTT.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<AppUser> Users { get; set; }
+    }
+}
