78f309e0077b8d5c2c82dd3399f605d4fc15c2ee
Naor Farcas <naor@korentec.co.il>
2020-10-19 09:55:29 +0300

    Add HTML direction switch depending on the translation language

--- KTT/ClientApp/src/app/app.component.ts
+++ KTT/ClientApp/src/app/app.component.ts
@@ -1,9 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'app';
+
+  constructor(@Inject(DOCUMENT) private document, private translate: TranslateService) {
+    // Set initial direction
+    this.changeDirection(translate.currentLang);
+
+    // Listen to language change and update direction if needed
+    translate.onLangChange.subscribe((event) => {
+      this.changeDirection(event.lang);
+    });
+  }
+
+  changeDirection(language: string) {
+    const lang = language;
+    const ltrrtl = lang === 'he' ? 'rtl' : 'ltr';
+
+    document.getElementsByTagName("html")[0].setAttribute('lang', lang);
+    document.getElementsByTagName("body")[0].setAttribute('dir', ltrrtl);
+  }
 }
