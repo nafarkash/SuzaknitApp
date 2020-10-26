a5e28e92849b9a42de84e6502e882896782e99f7
Naor Farcas <naor@korentec.co.il>
2020-10-26 10:16:02 +0200

    Enum description reading extension

---
+++ KTT/Extensions/EnumExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace KTT.Extensions
+{
+    public static class EnumExtensions
+    {
+        public static string ToDescriptionString<T>(this T val) where T: struct, IConvertible
+        {
+            if (!typeof(T).IsEnum)
+            {
+                throw new ArgumentException("T must be an enumerated type");
+            }
+
+            DescriptionAttribute[] attributes = (DescriptionAttribute[])val
+               .GetType()
+               .GetField(val.ToString())
+               .GetCustomAttributes(typeof(DescriptionAttribute), false);
+            return attributes.Length > 0 ? attributes[0].Description : string.Empty;
+        }
+    }
+}
